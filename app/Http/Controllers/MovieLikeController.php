34e33354ba5972bf66675a13aea2f0219e4516fc
Nenad Radulovic <popcornsftw2011@hotmail.com>
2020-12-21 16:30:35 +0100

    Code Refacture

--- app/Http/Controllers/MovieLikeController.php
+++ app/Http/Controllers/MovieLikeController.php
@@ -9,11 +9,6 @@ use App\Models\Movie;
 
 class MovieLikeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
-
     /**
      * Display a listing of the resource.
      *
--- routes/api.php
+++ routes/api.php
@@ -18,32 +18,21 @@ use App\Http\Controllers\CommentController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::resources([
     'user' => UserController::class,
     'movies' => MovieController::class
 ]);
-
+Route::post('login', [AuthController::class, 'login']);
 Route::group([
-    'prefix' => 'auth'
-
-], function ($router) {
-
-    Route::post('login', [AuthController::class, 'login']);
-    
-    Route::group([
-        'middleware' => 'auth:api'
-    ], function() {
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::post('me', [AuthController::class, 'me']);
-        Route::post('likeDislike', [UserController::class], 'LikeDislikeMovie');
-    });
+    'middleware' => 'auth:api'
+], function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('likeDislike', [UserController::class], 'LikeDislikeMovie');
     Route::resources([
         'likeMovie' => MovieLikeController::class,
         'commentMovie' => CommentController::class
     ]);
 });
+
